da5dec538648c24f6e0c3335c8e6f5e0ef179913
Pedram Babazadeh <pedrambabazadeh@hotmail.com>
2023-10-12 16:56:44 +0200

    game developed and about me text added

--- src/Containers/about/About.tsx
+++ src/Containers/about/About.tsx
@@ -4,8 +4,20 @@ const About = () => {
   return (
     <div className='about'>
             <div className='about-container'>
-              Welcome to the about me part
-                
+            <header>
+        <h1>Pedram Babazadeh - Aspiring Junior Frontend Developer</h1>
+    </header>
+    <section>
+        <p>Hello, I'm Pedram Babazadeh, an aspiring Junior Frontend Developer driven by a passion for crafting immersive online experiences. With a strong foundation in frontend technologies, including JavaScript and React.js, I've earned certifications from Meta Ltd. in "Programming with JavaScript," "Advanced React.js," and "Principles of UI and UX."</p>
+
+        <p>My journey began in the humanities, where my studies in linguistics, politics, and sociology cultivated a deep appreciation for the intricacies of human communication. Today, I channel this passion into the digital realm, creating elegant and intuitive user interfaces that captivate and engage.</p>
+
+        <p>Outside of the digital canvas, I find joy in the cerebral challenges of chess and the exhilaration of football matches. Poetry provides me with a creative outlet to express the depth of human emotions, and movies serve as a wellspring of inspiration, broadening my horizons with diverse narratives.</p>
+
+        <p>As I set my sights on becoming a Junior Frontend Developer, I am actively seeking opportunities to fuse my love for humanities with the precision of coding. My ultimate goal is to explore the fascinating world of Computational Linguistics, an ambition I aim to pursue at a renowned EU university.</p>
+
+        <p>This website stands as a testament to my dedication, showcasing my digital creations meticulously crafted using React.js and TypeScript. Thank you for visiting, and I eagerly anticipate the exciting challenges and discoveries that lie ahead in my frontend development journey and my pursuit of Computational Linguistics in the EU.</p>
+    </section>               
             </div>
     </div>
   )
--- src/Containers/play-game/PlayGame.tsx
+++ src/Containers/play-game/PlayGame.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
-
+import React, { EffectCallback, useEffect, useState } from 'react'
 import './play-game.css'
 
+interface square {
+  left: number,
+  top: number
+}
+
 const PlayGame = () => {
+
+  const [squarePositions, setSquarePositions] = useState<square[]>([]);
+
+  useEffect(() => {
+    const container = document.querySelector('.rectangle') as HTMLElement | null;
+
+    const containerWidth = container?.offsetWidth;
+    const containerHeight = container?.offsetHeight;
+
+    const newSquarePositions : square[] = Array.from({ length: 8 }, () => {
+      const left = Math.random() * (containerWidth? containerWidth - 25 : 0); 
+      const top = Math.random() * (containerHeight? containerHeight - 25 : 0); 
+      return { left, top };
+    });
+
+    setSquarePositions(newSquarePositions);
+  } , []);
+
   return (
     <div>
-      <div className="rectangle"></div>
+      <div className="rectangle">
+        {squarePositions.map((square, index) =>{
+          
+          return (<div className="square-player" style={{top: square.top, left: square.left}}> </div>);
+        })}
+      </div>
     </div>
   )
 }
