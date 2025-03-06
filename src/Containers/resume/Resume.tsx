df3ce98e803edb08b4442661a3d8c2151a8b8efd
PEDRAM BABAZADEH <68613205+pedrambabazadeh@users.noreply.github.com>
2025-03-06 21:49:41 +0100

    cv dinimic object defined

--- src/Containers/resume/Resume.tsx
+++ src/Containers/resume/Resume.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './resume.css'
+import { cvData as CV } from './cvData'
 
 const Resume = () => {
   return (
@@ -21,19 +22,7 @@ const Resume = () => {
             Introduction
           </h2>        
           <p className='resume-intro-p resume-p'>
-          As a junior front-end developer, I am passionate
-about creating engaging and intuitive user
-interfaces that improve the user experience. I
-have experience with HTML, CSS, and JavaScript,
-having developed several projects including
-responsive websites and web applications. I have
-also worked with popular frontend frameworks
-and libraries such as React.js, jQuery, and
-Bootstrap, and am eager to continue learning and
-using new technologies. Collaborating with other
-team members is a strength of mine, and I am
-dedicated to writing clean and efficient code
-that meets project requirements.
+          {CV.header}
           </p>
 
         </section>
---
+++ src/Containers/resume/cvData.js
@@ -0,0 +1,17 @@
+export const cvData =
+{
+    header: `As a junior front-end developer, I am passionate
+    about creating engaging and intuitive user
+    interfaces that improve the user experience. I
+    have experience with HTML, CSS, and JavaScript,
+    having developed several projects including
+    responsive websites and web applications. I have
+    also worked with popular frontend frameworks
+    and libraries such as React.js, jQuery, and
+    Bootstrap, and am eager to continue learning and
+    using new technologies. Collaborating with other
+    team members is a strength of mine, and I am
+    dedicated to writing clean and efficient code
+    that meets project requirements.`,
+
+}
